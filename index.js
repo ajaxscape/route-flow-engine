21f0a9ce9aec2baa6e81ce4fff960eef31bfb138
ben <ben@ajaxscape.com>
2019-12-04 15:49:24 +0000

    Add tests

--- index.js
+++ index.js
@@ -1,13 +1,73 @@
+
+const merge = require('deepmerge')
+
 class RouteFlowEngine {
   constructor (options = {}) {
     const { config } = options
+
     if (config) {
-      this._config = config
+      this._config = merge({}, config)
     }
+
+    this._parseFlow(config)
+  }
+
+  _parseFlow (config) {
+    this._flow = merge({}, config)
+    Object.entries(this._flow).forEach(([id, node]) => {
+      const { next, title } = node
+      // Make sure the next attribute that when executed will return the next node
+      if (typeof next === 'string') {
+        node.next = async () => this._flow[next]
+      } else if (typeof next === 'object') {
+        const { query, when } = next
+        if (typeof query === 'function' && typeof when === 'object') {
+          node.next = async (...args) => {
+            const result = await query(...args)
+            if (result === undefined) {
+              throw new Error(`Expected the "next" query result for "${id}" to be one of "${Object.keys(when).join('", "')}" instead of "${result}"`)
+            }
+            if (Object.keys(when).includes(result.toString())) {
+              return this._flow[when[result]]
+            } else {
+              throw new Error(`Expected the "next" query result for "${id}" to be one of "${Object.keys(when).join('", "')}" instead of "${result}"`)
+            }
+          }
+        } else {
+          throw new Error(`Expected the "next" attribute for "${id}" to contain a "query" function and a "when" object`)
+        }
+      }
+
+      // Make sure the title attribute that when executed will return the next node
+      if (typeof title === 'string') {
+        node.title = async () => title
+      } else if (typeof title === 'object') {
+        const { query, when } = title
+        if (typeof query === 'function' && typeof when === 'object') {
+          node.title = async (...args) => {
+            const result = await query(...args)
+            if (result === undefined) {
+              throw new Error(`Expected the "title" query result for "${id}" to be one of "${Object.keys(when).join('", "')}" instead of "${result}"`)
+            }
+            if (Object.keys(when).includes(result.toString())) {
+              return when[result]
+            } else {
+              throw new Error(`Expected the "title" query result for "${id}" to be one of "${Object.keys(when).join('", "')}" instead of "${result}"`)
+            }
+          }
+        } else {
+          throw new Error(`Expected the "title" attribute for "${id}" to contain a "query" function and a "when" object`)
+        }
+      }
+    })
   }
 
   get config () {
     return this._config
+  }
+
+  get flow () {
+    return this._flow
   }
 }
 
--- index.test.js
+++ index.test.js
@@ -1,13 +1,79 @@
 const { RouteFlowEngine } = require('./index')
+const merge = require('deepmerge')
+
+const config = {
+  home: {
+    path: '/',
+    next: 'first-page'
+  },
+
+  'first-page': {
+    path: 'first-page',
+    title: 'The first page',
+    next: {
+      query: function (choice) { return choice },
+      when: {
+        false: 'second-page',
+        true: 'third-page'
+      }
+    }
+  },
+
+  'second-page': {
+    path: 'second-page',
+    title: {
+      query: function (choice) { return choice },
+      when: {
+        false: 'The second page',
+        true: 'The alternative second page'
+      }
+    },
+    next: 'third-page'
+  },
+
+  'third-page': {
+    path: 'third-page',
+    title: 'The third page',
+    next: 'first-page'
+  }
+}
 
 describe('RouteFlowEngine Test', () => {
   beforeEach(async () => {
-    const config = this.config = { cheese: true }
-    this.routeFlow = new RouteFlowEngine({ config })
+    this.config = merge({}, config)
+    this.routeFlow = new RouteFlowEngine({ config: this.config })
   })
 
-  it('should test that the config is returned correctly', async () => {
-    const { routeFlow, config } = this
-    expect(routeFlow.config).toEqual(config)
+  it('config should be intact', async () => {
+    const { config } = this.routeFlow
+    expect(config).toEqual(this.config)
+  })
+
+  it('home should flow to first page', async () => {
+    const { flow } = this.routeFlow
+    const node = flow.home
+    expect(node.title).toEqual(undefined)
+    expect(await node.next()).toEqual(flow['first-page'])
+  })
+
+  it('first page should flow to second page', async () => {
+    const { flow } = this.routeFlow
+    const node = flow['first-page']
+    expect(await node.title()).toEqual('The first page')
+    expect(await node.next(true)).toEqual(flow['third-page'])
+  })
+
+  it('second page should flow to third page', async () => {
+    const { flow } = this.routeFlow
+    const node = flow['second-page']
+    expect(await node.title(false)).toEqual('The second page')
+    expect(await node.next()).toEqual(flow['third-page'])
+  })
+
+  it('third page should flow to first page', async () => {
+    const { flow } = this.routeFlow
+    const node = flow['third-page']
+    expect(await node.title()).toEqual('The third page')
+    expect(await node.next()).toEqual(flow['first-page'])
   })
 })
